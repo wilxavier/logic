f5c01b783d16d1ec41e8c3ce68dc3cf2f9e0bc58
William Xavier <william.santos@aluno.unb.br>
2024-06-30 14:27:46 -0300

    somador tentativas

--- logica-js-aula_2/app.js
+++ logica-js-aula_2/app.js
@@ -1,19 +1,22 @@
 alert('Boas vindas ao jogo do número secreto');
 let numeroSecreto = 5;
 console.log(numeroSecreto);
-let chute
+let chute = 0;
+let tentativas = 1;
 
 //enquanto chute nao for igual ao nr 5
 while (chute != numeroSecreto) {
     chute = prompt('Escolha um numero entre 1 e 10');
     //se chute for igual ao número secreto
     if (chute == numeroSecreto) {//quando usar o dolar tem que usar a crase
-        alert(`Isso ai! Voce descobriu o numero secreto ${numeroSecreto}`);
+        alert(`Isso ai! Voce descobriu o numero secreto ${numeroSecreto} com ${tentativas} tentativas.`);
     } else {
         if (chute > numeroSecreto) {
             alert(`O numero secreto é menor do que ${chute}`);
         } else {
             alert(`O numero secreto é maior do que ${chute}`)
         }
+        //tentativas = tentativas + 1
+        tentativas++;
     }
 }
