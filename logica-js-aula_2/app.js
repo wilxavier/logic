f3ce14657c3fc50297aac05487f545bcbfa9f132
William Xavier <william.santos@aluno.unb.br>
2024-06-30 14:15:40 -0300

    aula 2 - while

--- logica-js-aula_2/app.js
+++ logica-js-aula_2/app.js
@@ -1,16 +1,19 @@
 alert('Boas vindas ao jogo do número secreto');
 let numeroSecreto = 5;
 console.log(numeroSecreto);
-let chute = prompt('Escolha um número entre 1 e 10');
+let chute
 
-
-//se chute for igual ao número secreto
-if(chute==numeroSecreto){//quando usar o dolar tem que usar a crase
-    alert(`Isso ai! Voce descobriu o numero secreto ${numeroSecreto}`);
-} else {
-    if (chute > numeroSecreto){
-        alert(`O numero secreto é menor do que ${chute}`);
+//enquanto chute nao for igual ao nr 5
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um numero entre 1 e 10');
+    //se chute for igual ao número secreto
+    if (chute == numeroSecreto) {//quando usar o dolar tem que usar a crase
+        alert(`Isso ai! Voce descobriu o numero secreto ${numeroSecreto}`);
     } else {
-        alert(`O numero secreto é maior do que ${chute}`)
+        if (chute > numeroSecreto) {
+            alert(`O numero secreto é menor do que ${chute}`);
+        } else {
+            alert(`O numero secreto é maior do que ${chute}`)
+        }
     }
 }
