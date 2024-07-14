3315f2531e0b9793f7cc2750ca4f8b79d6626f4e
wilxavier <80928268+wilxavier@users.noreply.github.com>
2024-07-14 06:30:37 -0300

    limpar campo

--- js-curso-2-aula_3/app.js
+++ js-curso-2-aula_3/app.js
@@ -24,11 +24,17 @@ function verificarChute() {
             exibirTextoNaTela('p','O número secreto é maior');
         }
         tentativas++;
+        limparCampo()
     }
 }
 
 function gerarNumeroAleatorio() {
     return parseInt(Math.random() * 10 + 1);
+}
+
+function limparCampo(){
+    chute = document.querySelector('input');
+    chute.value = '';
 }
 
 
